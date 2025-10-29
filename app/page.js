a08e9979b070242a863fc68d6325134b62d65f33
Vasylj Sokoliuk <vasylj.sokoliuk.bohda@fclnup.if.ua>
2025-10-29 12:23:16 +0200

    second commit

--- app/page.js
+++ app/page.js
@@ -1,26 +1,36 @@
-import Link from "next/link";
+import Link from 'next/link'; 
 
-export default function Home() {
+const ExampleComponent = () => {
   return (
-   <>
-        
-         <h1>Привіт!</h1>
+    <>
+      <h1>Привіт!</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="/about"
-          >
-           Про нас
-          </Link>
-          <Link
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-          
+      <div className="flex gap-4 items-center flex-col sm:flex-row">
+  
+        <Link
+          href="/about"
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+        >
+          Про нас
+        </Link>
+
+        <Link
           href="/contact"
-          >
-           Контакти
-          </Link>
-        </div>
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+        >
+          Контакти
+        </Link>
+
+       
+        <Link
+          href="/dialog" 
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#ff6347] text-white gap-2 hover:bg-[#e55342] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+        >
+          Діалог
+        </Link>
+      </div>
     </>
   );
-}
+};
+
+export default ExampleComponent;
